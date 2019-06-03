2a90090dafff8f27a9faf20f89f9bac45cb5fd8a
Robert Savela <strobbann@gmail.com>
2019-06-03 16:30:51 +0200

    Added optional with null check on get method in AccountDao

--- src/main/java/se/rosa/dao/AccountDaoImpl.java
+++ src/main/java/se/rosa/dao/AccountDaoImpl.java
@@ -2,29 +2,34 @@ package se.rosa.dao;
 
 import se.rosa.domain.Account;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class AccountDaoImpl implements AccountDao {
 
-	private Map<Long, Account> accounts = new HashMap<>();
+	private Map<Long, Account> accounts;
+
+	public AccountDaoImpl() {
+		this.accounts = new HashMap<>();
+	}
 
 	@Override
 	public void create(Account account) {
-		accounts.put(account.getId(), account);
+		Optional<Account> accountOptional = Optional.of(account);
+		accountOptional.ifPresent(a -> accounts.put(a.getId(), a));
 	}
 
 	@Override
 	public Account get(Long id) {
-		return accounts.get(id);
+		return Optional.of(accounts.get(id))
+				.map(account -> account)
+				.orElseThrow(()-> new IllegalArgumentException("Cannot find account with this id"));
 	}
 
 	@Override
 	public void update(Account account) {
+		Objects.requireNonNull(account,"Account to update cannot be null");
 		if (accounts.containsKey(account.getId())) {
 			accounts.replace(account.getId(), account);
 		}
@@ -32,6 +37,7 @@ public class AccountDaoImpl implements AccountDao {
 
 	@Override
 	public void remove(Long id) {
+		Objects.requireNonNull(id,"Id to remove cannot be null");
 		if(accounts.containsKey(id)) {
 			accounts.remove(id);
 		}
