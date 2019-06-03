8d7f9e516a9f11e12632afbc382276856d024a23
Robert Savela <strobbann@gmail.com>
2019-06-03 16:31:28 +0200

    Changed remove test to check for exception when not found

--- src/test/java/se/rosa/dao/AccountDaoUnitTest.java
+++ src/test/java/se/rosa/dao/AccountDaoUnitTest.java
@@ -1,12 +1,17 @@
 package se.rosa.dao;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import se.rosa.domain.Account;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class AccountDaoUnitTest {
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
 
 	@Test
 	public void create() {
@@ -17,13 +22,14 @@ public class AccountDaoUnitTest {
 	}
 
 	@Test
-	public void remove() {
+	public void removeThrowsExceptionWhenNotFound() {
 		AccountDao accountDao = new AccountDaoImpl();
 		Account account = Account.builder().withId(1L).withBalance(200d).build();
 		accountDao.create(account);
 		assertEquals(account, accountDao.get(1L));
 		accountDao.remove(1L);
-		assertNotEquals(account, accountDao.get(1L));
+		expectedException.expect(NullPointerException.class);
+		accountDao.get(1L);
 	}
 
 	@Test
